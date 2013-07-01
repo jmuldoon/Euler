99e9393948b63c901115e12ee34f6cfb1afef74b
jmuldoon <jpm566@nyu.edu>
2013-06-30 23:04:58 -0400

    segfault 11, but cleaned up many warnings

--- q3.c
+++ q3.c
@@ -6,37 +6,40 @@
 
  // Header declarations
 #include <stdio.h>
-#include <cmath.h>
-#define VAL 600851475143;
+#include <stdlib.h>
+#include <math.h>
+#define VAL 13195;
 
 // Global variable declarations
 int *pf;	// prime factor array
 
 // Function prototype declarations
 int IsPrime(int);				// Determines if param is prime or not. returns 1 if prime, else 0
-int* PrimeFactorization(int); 	// Determines the prime factors for a give value param: v
+int* PrimeFactorization(long long int); 	// Determines the prime factors for a give value param: v
 int GPF(int*);					// Calculates the greatest prime factor (GPF)
 
 int main(int argc, char **argv){
-	v = VAL;
-	
-	pf = (int*)malloc((sqrt(v)/2)); // Allocates 1/2 the sqrt of v to the array of prime factors.
+	long long int v;
+	 v = VAL;
+
+	pf = (int*)malloc(5); // Allocates 1/2 the sqrt of v to the array of prime factors.
 	pf = PrimeFactorization(v);		// Obtain the prime factors for the value in v.
-	printf("Greatest prime factor of %d, is: %d", v, GPF());	// Display solution
-	
+	printf("Greatest prime factor of %lld, is: %d", v, GPF(pf));	// Display solution
+
 	return 0;
 }
 
-int* PrimeFactorization(int v){
-	int ch, *pfArray;
-	
-	for (ch = 2; ch <= (int)sqrt(v); ch++){	// Checks all possibilities for 2 through sqrt(v)
+int* PrimeFactorization(long long int v){
+	int ch, i;
+	int *pfArray;
+
+	for (ch = 2, i = 0; ch <= (int)sqrt(v); ch++){	// Checks all possibilities for 2 through sqrt(v)
 		if(IsPrime(ch)){			// is the count prime, if so execute the code
 			if(v%ch == 0){			// if the count goes in evenly to the current value
-				pfArray[cntr++] = ch;
+				pfArray[i++] = ch;
 				v = v/ch;
 				if (IsPrime(v)){	// if this is prime, then v is fully factored
-					pfArray[cntr++] = v;
+					pfArray[i] = v;
 					return pfArray;
 				}
 				else				// if not prime, then continue to find the next prime factor
@@ -51,7 +54,7 @@ int GPF(int *p){
 
 	if((sizeof(p)/sizeof(int))<=1) // if the size of the array is !larger than 1 prime factor
 		return p[0];
-	
+
 	max = p[0];
 	for (i = 0; i < sizeof(p)/sizeof(int); i++)	// Checks for all values in the array
 		max = fmax(max, p[i+1]);				// keeps only the max value
